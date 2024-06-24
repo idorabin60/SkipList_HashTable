1de77286bcb9f8f5b1cda996a5db715567ab1d74
ido rabin <idorabin@idos-MacBook-Pro-2.local>
2024-06-24 16:55:13 +0300

    finish 3.4

--- ProbingHashTable.java
+++ ProbingHashTable.java
@@ -10,7 +10,7 @@ public class ProbingHashTable<K, V> implements HashTable<K, V> {
     private int capacity;
     private HashFunctor<K> hashFunc;
     private Element<K, V>[] table;
-    private double laodFactor;
+    private double loadFactor;
     private int size;
 
     /*
@@ -27,88 +27,80 @@ public class ProbingHashTable<K, V> implements HashTable<K, V> {
         this.capacity = 1 << k;
         this.hashFunc = hashFactory.pickHash(k);
         this.table = new Element[capacity];
-        this.laodFactor = 0;
+        this.loadFactor = 0;
         this.size = 0;
-
     }
 
     public V search(K key) {
         int elementIndex = Math.abs(this.hashFunc.hash(key)) % this.capacity;
         int initialIndex = elementIndex;
         while (true) {
-            if (this.table[elementIndex].key() == key) {
-                return this.table[elementIndex].satelliteData();
-            }
             if (this.table[elementIndex] == null) {
                 return null;
+            }
+            if (this.table[elementIndex].key() != null && this.table[elementIndex].key().equals(key)) {
+                return this.table[elementIndex].satelliteData();
             } else {
-                elementIndex = HashingUtils.mod(initialIndex + 1, this.capacity);
+                elementIndex = HashingUtils.mod(elementIndex + 1, this.capacity);
                 if (elementIndex == initialIndex) {
                     return null;
-
                 }
             }
-
         }
-
     }
 
     public Element<K, V> searchForDelete(K key) {
         int elementIndex = Math.abs(this.hashFunc.hash(key)) % this.capacity;
         int initialIndex = elementIndex;
         while (true) {
-            if (this.table[elementIndex].key() == key) {
-                return this.table[elementIndex];
-            }
             if (this.table[elementIndex] == null) {
                 return null;
+            }
+            if (this.table[elementIndex].key() != null && this.table[elementIndex].key().equals(key)) {
+                return this.table[elementIndex];
             } else {
-                elementIndex = HashingUtils.mod(initialIndex + 1, this.capacity);
+                elementIndex = HashingUtils.mod(elementIndex + 1, this.capacity);
                 if (elementIndex == initialIndex) {
                     return null;
-
                 }
             }
-
         }
-
     }
 
     public void insert(K key, V value) {
-        if (this.laodFactor >= maxLoadFactor) {
-            this.rehas();
-        } else {
-            Element<K, V> elementToBeInserted = new Element<>(key, value);
-            int indexToBeInserted = Math.abs(this.hashFunc.hash(key)) % this.capacity;
-            while (true) {
-                if (this.table[indexToBeInserted] == null || this.table[indexToBeInserted].key() == null) {
-                    this.table[indexToBeInserted] = elementToBeInserted;
-                    break;
-                } else {
-                    indexToBeInserted = HashingUtils.mod(indexToBeInserted + 1, this.capacity);
-                }
-
+        if (this.loadFactor >= maxLoadFactor) {
+            this.rehash();
+        }
+        Element<K, V> elementToBeInserted = new Element<>(key, value);
+        int indexToBeInserted = Math.abs(this.hashFunc.hash(key)) % this.capacity;
+        while (true) {
+            if (this.table[indexToBeInserted] == null || this.table[indexToBeInserted].key() == null) {
+                this.table[indexToBeInserted] = elementToBeInserted;
+                break;
+            } else {
+                indexToBeInserted = HashingUtils.mod(indexToBeInserted + 1, this.capacity);
             }
         }
         this.size++;
-        this.laodFactor = this.size / this.capacity;
+        this.loadFactor = (double) this.size / this.capacity;
     }
 
-    public void rehas() {
+    public void rehash() {
         int newCapacity = this.capacity * 2;
         Element<K, V>[] newTable = new Element[newCapacity];
-        HashFunctor<K> newHashFunction = hashFactory.pickHash(newCapacity);
+        int newK = (int) (Math.log(newCapacity) / Math.log(2));
+        HashFunctor<K> newHashFunction = hashFactory.pickHash(newK);
         for (int i = 0; i < this.table.length; i++) {
             if (this.table[i] == null || this.table[i].key() == null) {
                 continue;
             } else {
-                int indexToBeInserted = Math.abs(newHashFunction.hash(this.table[i].key())) % this.capacity;
+                int indexToBeInserted = Math.abs(newHashFunction.hash(this.table[i].key())) % newCapacity;
                 while (true) {
                     if (newTable[indexToBeInserted] == null) {
                         newTable[indexToBeInserted] = this.table[i];
                         break;
                     } else {
-                        indexToBeInserted = HashingUtils.mod(indexToBeInserted + 1, this.capacity);
+                        indexToBeInserted = HashingUtils.mod(indexToBeInserted + 1, newCapacity);
                     }
                 }
             }
@@ -116,14 +108,15 @@ public class ProbingHashTable<K, V> implements HashTable<K, V> {
         this.capacity = newCapacity;
         this.table = newTable;
         this.hashFunc = newHashFunction;
-        this.laodFactor = this.laodFactor / 2.0;
-
+        this.loadFactor = (double) this.size / this.capacity;
     }
 
     public boolean delete(K key) {
         Element<K, V> elementToDelete = searchForDelete(key);
         if (elementToDelete != null) {
             elementToDelete.setKey(null);
+            this.size--;
+            this.loadFactor = (double) this.size / this.capacity;
             return true;
         }
         return false;
@@ -137,6 +130,3 @@ public class ProbingHashTable<K, V> implements HashTable<K, V> {
         return capacity;
     }
 }
-
-// nomodule in hash
-// trying to reach a key attribute of a null
